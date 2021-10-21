733145e95e14e097ccd80ae0cb59f5e76d8821ea
Matthew L. Komar <46490210+MatthewLKomar@users.noreply.github.com>
2021-10-21 19:25:12 -0400

    got to visualize steps. Now needs animations

--- p5jsVS_template/P5_CMU/Visualizer.js
+++ p5jsVS_template/P5_CMU/Visualizer.js
@@ -1,11 +1,4 @@
-﻿activate = false;
-
-
-function preload() {
-    //inconsolata = loadFont('DewiScript.otf');
-}
-
-
+﻿
 class Point {
     constructor(x, y, payload) {
         this.x = x;
@@ -28,9 +21,7 @@ var tileSize = new Point(20, 40);
 var coordinates = [];
 var coordSize = 0;
 
-var vecA;
-var vecB;
-var vecC;
+var activate = false;
 
 function draw() {
     if (activate) {
@@ -45,31 +36,21 @@ function setup() {
 
 function keyPressed() {
     if (keyCode == TAB && !activate) {
-        matricesGrids();
-        //populateGrid();
         activate = true;
     }
 }
 
-function matricesGrids() {
-    var rows = vectorA.length;
-    //+2 to have operation and equal sign
-    var cols = vectorA[0].length + vectorB[0].length + vectorC[0].length + 2;
-    for (row = 0; row < rows; row++) {
-        for (col = 0; col < cols; col++) {
-            coordinates[coordSize] = new Point(tileSize.x * col, tileSize.y * row);
-            //ellipse(coordinates[coordSize].x, coordinates[coordSize].y, 5, 5);
-            coordSize++;
-        }
-    }
-
-
-}
-//TODO: RETURN vec (1d array, size)
+//Visualize VectorA and VectorB 
+/*
+    * this displays the vector that's inputted and returns the shape of it
+    * looks like this: 
+    * [ [Point(coordX, coordY, payload), [Point(coordX, coordY, payload)] ..]
+    * Where payload is the value in this. 
+    */
 function populateVector(vector, offset) {
     var rows = vector.length;
     var cols = vector[0].length;
-    var arr =[]; 
+    var arr = [];
     for (row = 0; row < rows; row++) {
         arr[row] = new Array();
         for (col = 0; col < cols; col++) {
@@ -77,9 +58,11 @@ function populateVector(vector, offset) {
             text(vector[row][col], arr[row][col].x, arr[row][col].y);
         }
     }
-    return new Vec(arr, new Point(cols,rows));
+    return new Vec(arr, new Point(cols, rows));
 }
 
+
+//places offset distance from the vector to the left of it. 
 function placeSign(vector, sign, offset) {
     var rows = vectorA.length;
     var cols = vectorA[0].length;
@@ -107,18 +90,43 @@ function placeSign(vector, sign, offset) {
     text(S, (tileSize.x * (cols - 1)) + offset.x, tileSize.y * (rows - 1) + offset.y);
 }
 
+//outputs the matrices equation... there is probably a better way to do this but I'm not sure right now.
 function populateGrid() {
     offsetX = 15;
-    vecA = populateVector(vectorA, new Point(10, 50));
-    placeSign(vectorA, operation, new Point(25, 30)); //TODO: GET OPERAND FROM vecVis. 
-    vecB = populateVector(vectorB, new Point(60, 50));
+    var vecA = populateVector(vectorA, new Point(10, 50));
+    placeSign(vectorA, operation, new Point(25, 30));
+    var vecB = populateVector(vectorB, new Point(60, 50));
     placeSign(vectorA, operations.EQUAL, new Point(60 + offsetX, 30));
+    visualizeAddSub(vecA, vecB, new Point(110, 50), true, true);
 }
 
-function startVis() {
-    //put circle over element in one array
-    //put circle over element in another array
+function visualizeAddSub(vecA, vecB, offset, add, vis) {
+    //TODO: write up some assertions that the dimensions are the same and other rules for this.
+    vecC = [];
+    for (var row = 0; row < vecA.size.y; row++) {
+        vecC[row] = new Array();
+        for (var col = 0; col < vecA.size.x; col++) {
+            if (vis) {
+                var payload; 
+                if (add) {
+                    payload = String(vecA.vec[row][col].payload + "+" + vecB.vec[row][col].payload);
+                }
+                else {
+                    payload = String(vecA.vec[row][col].payload + "-" + vecB.vec[row][col].payload);
+                }
+                vecC[row][col] = new Point((tileSize.x * col) + offset.x, tileSize.y * row + offset.y, payload);
+            }
+            else {
+                if (add) payload = (vecA.vec[row][col].payload + vecB.vec[row][col].payload);
+                else payload = (vecA.vec[row][col].payload - vecB.vec[row][col].payload);
+                vecC[row][col] = new Point((tileSize.x * col) + offset.x, tileSize.y * row + offset.y, payload);
+            }
+            text(payload, vecC[row][col].x + (20 * col), vecC[row][col].y);
 
+        }
+    }
+    return new Vec(vecC, new Point(vecA.size.x, vecA.size.y));
 }
+
 
 
